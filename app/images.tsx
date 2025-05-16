580a3f9a4fa205137aaf8d172d30daed99f922ff
Said Akhrouf <Akhrouf@gmail.com>
2025-05-16 11:20:51 +0100

    affichage des images

---
+++ app/images.tsx
@@ -0,0 +1,2 @@
+export const Logo = require('@/assets/images/logo.png');
+export const LOGO_URI = "https://www.ibnrochd.com/images/leader.png"
--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,8 @@
+import { Image } from "expo-image";
 import { Link } from "expo-router";
 import { useState } from "react";
 import { Button, Text, View } from "react-native";
+import { LOGO_URI, Logo } from "./images";
 
 export default function Index() {
   const [x, setX] = useState(0);
@@ -12,6 +14,8 @@ export default function Index() {
         alignItems: "center",
       }}
       >
+      <Image source={Logo} style={{width:180, height:60}} />
+      <Image source={{uri: LOGO_URI}} style={{width:180, height:60}} />
       <Text>Vous avez cliqué { x } fois</Text>
       <Button title="+" onPress={()=>setX(x+1)} />
       <Link href="/page2" >PAGE 2</Link>
