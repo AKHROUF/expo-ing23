74207b5c6668c16b86829e218e479c6345ac7ae7
Said AKHROUF <akhrouf@gmail.com>
2025-05-24 10:44:59 +0100

    Boite de dialog

--- app/(tab)/index.tsx
+++ app/(tab)/index.tsx
@@ -1,7 +1,9 @@
 import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { useState } from "react";
+import { Button, Modal, StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
+  const [visible, setVisible] = useState(false);
   return (
     <View
       style={{
@@ -12,6 +14,57 @@ export default function Index() {
     >
       <Text>AAAAAAAA</Text>
       <Link href="/page2" >PAGE 2</Link>
+      <Button title="OK" onPress={() => setVisible(true)} />
+
+      <Modal visible={visible} //transparent={true} 
+            onRequestClose={() => setVisible(false)} >
+        <View style={styles.modalOverlay}>
+          <View style={styles.modalBox}>
+            <Text style={styles.modalTitle}>GeeksforGeeks</Text>
+            <Text style={styles.modalText}>How are you!</Text>
+            <View style={styles.buttonRow}>
+              <Button title="Cancel" onPress={() => setVisible(false)} />
+              <Button title="OK" onPress={() => setVisible(false)} />
+            </View>
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  modalOverlay: {
+    flex: 1,
+    backgroundColor: "rgba(0,0,0,0.4)",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  modalBox: {
+    backgroundColor: "white",
+    padding: 20,
+    borderRadius: 10,
+    width: 300,
+    alignItems: "center",
+  },
+  modalTitle: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+  modalText: {
+    fontSize: 16,
+    marginBottom: 20,
+  },
+  buttonRow: {
+    flexDirection: "row",
+    gap: 4,
+    justifyContent: "space-between",
+    width: "100%",
+  },
+});
