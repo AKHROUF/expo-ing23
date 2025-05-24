2540b635ba24e09a63c24fd3cb8e43dbf93b4985
Said AKHROUF <akhrouf@gmail.com>
2025-05-24 10:45:20 +0100

    formulaire

--- app/(tab)/index2.tsx
+++ app/(tab)/index2.tsx
@@ -1,7 +1,19 @@
 import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { useState } from "react";
+import { Alert, Button, Text, TextInput, View } from "react-native";
 
 export default function Index() {
+  const [nom, setNom] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+  const send = () => {
+    if (!nom || !email || !message) {
+      Alert.alert('Erreur', 'Tous les champs sont obligatoires.');
+      return;
+    }
+    Alert.alert('Formulaire envoyé', `Nom : ${nom}\nEmail : ${email}\nMessage : ${message}`);
+  };
   return (
     <View
       style={{
@@ -10,7 +22,11 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>BBBBBBB</Text>
+      <Text >Formulaire de contact</Text>
+      <TextInput  placeholder="Nom"     value={nom}     onChangeText={setNom}  />
+      <TextInput  placeholder="Email"   value={email}   onChangeText={setEmail} keyboardType="email-address" />
+      <TextInput  placeholder="Message" value={message} onChangeText={setMessage} multiline />
+      <Button title="Envoyer" onPress={send} />
       <Link href="/page2" >PAGE 2</Link>
     </View>
   );
