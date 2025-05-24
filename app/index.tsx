14fb7984ca0e82c443c672a0f6724eaac2b51391
Said AKHROUF <akhrouf@gmail.com>
2025-05-24 10:44:21 +0100

    Affichage d'un message (Alert)

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,17 @@
 import { Image } from "expo-image";
 import { Link } from "expo-router";
 import { useState } from "react";
-import { Button, Text, View } from "react-native";
+import { Alert, Button, Text, View } from "react-native";
 import { LOGO_URI, Logo } from "./images";
 
 export default function Index() {
   const [x, setX] = useState(0);
+  const alert = () => {
+    Alert.alert("Gestion RH", "Supprimer cet utilisateur ?", 
+      [{ text: "Cancel", }, { text: "OK", onPress:supprimer}]);
+  }
+  const supprimer = (a?: string)=> {console.log(a);}
+
   return (
     <View
       style={{
@@ -16,8 +22,9 @@ export default function Index() {
       >
       <Image source={Logo} style={{width:180, height:60}} />
       <Image source={{uri: LOGO_URI}} style={{width:180, height:60}} />
-      <Text>Vous avez cliqué { x } fois</Text>
+      <Text>Vous avez cliqué { x } fois sur le bouton</Text>
       <Button title="+" onPress={()=>setX(x+1)} />
+      <Button title="message" onPress={alert} />
       <Link href="/page2" >PAGE 2</Link>
       <Link href="/(tab)" >PAGE 3</Link>
     </View>
